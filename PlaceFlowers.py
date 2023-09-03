2c0c4c84a5ee731a2c16de780a68f8c90fd7a761
abrehan2 <rehannajam2@gmail.com>
2023-09-03 16:14:54 +0500

    initial commit

---
+++ PlaceFlowers.py
@@ -0,0 +1,13 @@
+def canPlaceFlowers(flowerbed, n):
+
+    f = [0] + flowerbed + [0]
+
+    for i in range(1, len(flowerbed) - 1):
+        if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+             f[i] = 1
+             n -= 1
+
+    return n<=0
+
+
+print(canPlaceFlowers([1,0,0,0,1], 2))
