1ccc0fe90ef1f3058d8e8625147bdd3d10c483dd
abrehan2 <rehannajam2@gmail.com>
2023-09-18 19:39:39 +0500

    Updated ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -4,19 +4,18 @@
 #         self.next = next
 
 
-
 class Solution:
     def reverse_list(self, head):
         prev, current = None, head
-        
+
         while current:
+            temp = current.next
             current.next = prev
             prev = current
-            current = current.next
-        
+            current = temp
+
         return prev
 
-        
 
 # sol_obj = Solution();
 # sol_obj.reverse_list([1,2,3,4,5])
